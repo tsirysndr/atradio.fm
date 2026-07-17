2b9658b2d8a5788827efdc79f0060d5867e83366
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-17 14:03:10 +0300

    chore: bump version to 0.2.0

--- cli/dist/homebrew/atradio.rb
+++ cli/dist/homebrew/atradio.rb
@@ -5,7 +5,7 @@
 class Atradio < Formula
   desc "A TUI radio player on the AT Protocol"
   homepage "https://github.com/tsirysndr/atradio.fm"
-  version "0.1.0"
+  version "0.2.0"
   license "GPL-2.0-or-later"
 
   on_macos do
--- cli/dist/rpm/amd64/atradio.spec
+++ cli/dist/rpm/amd64/atradio.spec
@@ -1,5 +1,5 @@
 Name:           atradio
-Version:        0.1.0
+Version:        0.2.0
 Release:        1%{?dist}
 Summary:        A TUI radio player on the AT Protocol
 
